33bc5a8a8f21a140fa9f4d17b8de90d3e9805cf3
WoutProvost <wout_provost@hotmail.com>
2021-02-18 04:00:14 +0100

    Switch to H2 from SQLite, since paging didn't work

--- src/main/java/com/example/horeca/Application.java
+++ src/main/java/com/example/horeca/Application.java
@@ -1,13 +1,13 @@
 package com.example.horeca;
 
-// import com.example.horeca.domain.Horeca;
-// import com.example.horeca.services.HorecaService;
-// import java.io.IOException;
-// import java.io.InputStream;
-// import com.fasterxml.jackson.core.type.TypeReference;
-// import com.fasterxml.jackson.databind.ObjectMapper;
-// import org.springframework.boot.CommandLineRunner;
-// import org.springframework.context.annotation.Bean;
+import com.example.horeca.domain.Horeca;
+import com.example.horeca.services.HorecaService;
+import java.io.IOException;
+import java.io.InputStream;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -19,26 +19,26 @@ public class Application {
 	}
 
 	// Deserialize JSON and store objects in database
-	// @Bean
-	// public CommandLineRunner initializeDatabase(HorecaService service) {
-	// 	return args -> {
-	// 		ObjectMapper mapper = new ObjectMapper();
-	// 		TypeReference<Iterable<Horeca>> typeReference = new TypeReference<Iterable<Horeca>>(){};
-	// 		InputStream inputStream = TypeReference.class.getResourceAsStream("/data.json");
+	@Bean
+	public CommandLineRunner initializeDatabase(HorecaService service) {
+		return args -> {
+			ObjectMapper mapper = new ObjectMapper();
+			TypeReference<Iterable<Horeca>> typeReference = new TypeReference<Iterable<Horeca>>(){};
+			InputStream inputStream = TypeReference.class.getResourceAsStream("/data.json");
 
-	// 		try {
-	// 			System.out.println("Saving data ...");
+			try {
+				System.out.println("Saving data ...");
 
-	// 			Iterable<Horeca> horeca = mapper.readValue(inputStream, typeReference);
+				Iterable<Horeca> horeca = mapper.readValue(inputStream, typeReference);
 
-	// 			for(Horeca h : horeca) {
-	// 				service.save(h);
-	// 			}
+				for(Horeca h : horeca) {
+					service.save(h);
+				}
 
-	// 			System.out.println("Data saved.");
-	// 		} catch(IOException e) {
-	// 			System.err.println("Unable to save data: " + e.getMessage());
-	// 		}
-	// 	};
-	// }
+				System.out.println("Data saved.");
+			} catch(IOException e) {
+				System.err.println("Unable to save data: " + e.getMessage());
+			}
+		};
+	}
 }
